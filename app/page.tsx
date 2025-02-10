79656dd091a671bd55af54630725dc5793005834
yoonustehrani <yoonustehrani28@gmail.com>
2025-02-10 23:40:58 +0000

    phone buttons responsive

--- app/page.tsx
+++ app/page.tsx
@@ -33,16 +33,16 @@ export default function Home() {
 						<h1 className="text-5xl mb-20 w-full text-center md:w-fit font-bold text-eerieBlack dark:text-antiFlashWhite relative">کاکتوس&nbsp;<span aria-label="تور" className="-mr-28 mt-12 absolute inline-block">تـــور</span></h1>
 						{/* <p className="text-xl text-justify">برگزار کننده تور های داخلی و خارجی از مبدا مشهد</p>
 						 */}
-						<h2 className="text-2xl text-justify">کاکتوس، مجری تخصصی <strong>تورهای داخلی و خارجی</strong> از مبدا مشهد</h2>
-						<p className="text-xl text-justify">با سال‌ها تجربه در صنعت گردشگری، بهترین و متنوع‌ترین تورها را به مقاصد محبوب داخلی و خارجی ارائه می‌دهیم. تورهای کاکتوس با برنامه‌ریزی حرفه‌ای، <em className="underlined">خدمات باکیفیت و قیمت‌های رقابتی</em>، سفری به‌یادماندنی را برای شما رقم می‌زند.</p>
+						<h2 className="text-2xl text-justify">کاکتوس، مجری تخصصی <strong>تورهای داخلی و خارجی</strong>&nbsp;<span aria-label="airplane emoji">✈️</span> از مبدا مشهد</h2>
+						<p className="text-xl text-justify">با سال‌ها تجربه در صنعت گردشگری، بهترین و متنوع‌ترین تورها را به مقاصد محبوب داخلی و خارجی ارائه می‌دهیم. تورهای کاکتوس با برنامه‌ریزی حرفه‌ای، <em className="underlined">خدمات باکیفیت و قیمت‌های رقابتی</em> <span aria-label="Money bag">💰</span>، سفری به‌یادماندنی را برای شما رقم می‌زند.</p>
 						<p className="text-xl text-justify">برای اطلاع از آخرین تورها و تخفیف‌های ویژه، از گزینه های زیر استفاده کنید:</p>
-						<div className="flex text-lg items-end gap-4">
-							<button className="bg-butterscotch border-raw-umber dark:bg-darkBlue-marian-light dark:border-darkBlue-marian-dark border-b-4 border-r-4 px-3 py-1 rounded-md flex items-center gap-2">
+						<div className="flex flex-col items-center sm:flex-row text-lg gap-4">
+							<button className="bg-butterscotch border-raw-umber dark:bg-darkBlue-marian-light dark:border-darkBlue-marian-dark border-b-4 border-r-4 px-3 py-1 rounded-md flex items-center gap-2"> {/** min-w-44 md:w-auto justify-between md:justify-start */}
 								<span className="inline-flex w-8 h-8 bg-black/10 rounded-full shadow-inner shadow-black/20"></span>
 								جستجوی تور
 							</button>
-							<button className="bg-appleGreen dark:bg-avocado border-dark-moss-green border-b-4 border-r-4 px-2 py-1 rounded-md flex items-center gap-2">
-								<span className="inline-flex w-7 h-7 bg-black/10 rounded-full shadow-inner shadow-black/20"></span>
+							<button className="bg-appleGreen dark:bg-avocado border-dark-moss-green border-b-4 border-r-4 px-2 py-1 rounded-md flex items-center gap-2"> {/** min-w-44 md:w-auto justify-between md:justify-start */}
+								<span className="inline-flex w-8 h-8 bg-black/10 rounded-full shadow-inner shadow-black/20"></span>
 								تماس با کاکتوس
 							</button>
 						</div>
