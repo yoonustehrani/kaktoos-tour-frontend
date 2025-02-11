2c0a8627cf7ba472080da41909835d19090dd4fb
yoonustehrani <yoonustehrani28@gmail.com>
2025-02-11 17:53:28 +0000

    fixed animation initial lag

--- app/ui/forms/SearchTourForm.tsx
+++ app/ui/forms/SearchTourForm.tsx
@@ -4,11 +4,14 @@ import './style.css';
 
 export default function SearchTourForm() {
     return (
-        <div id='search-tour-form' className="bg-black/20 backdrop-blur-lg shadow-lg rounded-md w-4/5 h-96 -mt-16">
+        <div id='search-tour-form' className="bg-black/20 backdrop-blur-lg shadow-lg rounded-md w-4/5 h-96 -mt-16 relative">
             <span></span>
             <span></span>
             <span></span>
             <span></span>
+            <div className='w-full h-full'>
+
+            </div>
         </div>
     )
 }
