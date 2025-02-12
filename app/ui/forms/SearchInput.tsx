0c49fc7b796b05278d9874b063493f6c84236125
yoonustehrani <yoonustehrani28@gmail.com>
2025-02-12 19:42:28 +0000

    search input autofocus disabled

--- app/ui/forms/SearchInput.tsx
+++ app/ui/forms/SearchInput.tsx
@@ -7,7 +7,7 @@ export default function SearchInput() {
     return (
         <div className='w-full text-lg flex items-center relative'>
             <span className={`left-3 absolute fi fi-rs-${loading ? 'spinner animate-spin' : 'search'} size-4`}></span>
-            <input onKeyUp={() => setLoading(true)} dir='rtl' type="text" placeholder='مبدا را جستجو کنید' className='w-full pr-1 pl-8 py-2 text-right bg-transparent border-b-2 border-gray-300 dark:border-gray-700 focus:border-gray-700 dark:focus:border-darkBlue-delft'/>
+            <input autoFocus={false} onKeyUp={() => setLoading(true)} dir='rtl' type="text" placeholder='مبدا را جستجو کنید' className='w-full pr-1 pl-8 py-2 text-right bg-transparent border-b-2 border-gray-300 dark:border-gray-700 focus:border-gray-700 dark:focus:border-darkBlue-delft'/>
         </div>
     )
 }
