6d203d8bba1188ecf559324f2e3214e8adbfb0ea
yoonustehrani <yoonustehrani28@gmail.com>
2025-04-16 15:34:22 +0330

    mobile menu responsive

--- app/ui/nav/HasSubMenu.tsx
+++ app/ui/nav/HasSubMenu.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { ReactNode, TouchEvent, useEffect, useRef, useState } from "react";
 
 type Props = {
     title: string;
@@ -37,7 +37,7 @@ function SubMenuList({active, children}: {active: boolean; children: ReactNode})
 
 export default function HasSubMenu({ title }: Props) {
     const [display, setDisplay] = useState(false);
-    const [subMenu, setSubMenu] = useState<string>("country")
+    const [subMenu, setSubMenu] = useState<string>()
     const anchorRef = useRef<HTMLLIElement>(null); // Ref specifically for the <a> tag
 
     useEffect(() => {
@@ -45,6 +45,7 @@ export default function HasSubMenu({ title }: Props) {
         // Close if click is outside the anchor tag
         if (anchorRef.current && !anchorRef.current.contains(event.target)) {
             setDisplay(false);
+            setSubMenu(undefined);
         }
         }
 
@@ -68,17 +69,18 @@ export default function HasSubMenu({ title }: Props) {
                 <i className={`fi size-5 ${display ? 'fi-rs-angle-small-up' : 'fi-rs-angle-small-down'}`}></i>
             </a>
             {display && (
-                <div className="absolute z-50 bg-antiFlashWhite rounded-lg shadow-lg p-3 mt-2 flex gap-2 h-fit w-max">
-                    <div className="flex gap-3">
-                        <div className="w-max min-w-48 bg-white rounded-lg h-full p-3 flex flex-col gap-4">
+                <div className="fixed top-0 right-0 h-full bg-antiFlashWhite w-4/5 md:absolute md:top-auto md:right-auto z-50 md:h-fit md:w-max rounded-lg shadow-lg p-3 md:mt-2">
+                    {/*   flex gap-2  */}
+                    <div className="md:flex gap-3 w-full h-full md:w-fit md:h-fit">
+                        <div className="w-full md:w-max min-w-48 rounded-lg h-full p-3 flex flex-col justify-center gap-4">
                             <p className="text-sm font-semibold text-gray-700">{title}</p>
-                            <ul className="flex flex-col gap-2 min-w-full w-max text-xl grow">
-                                <li id="country" onMouseEnter={(e) => setSubMenu(e.currentTarget.id)} className={`flex gap-3 items-center ${subMenu === 'country' ? 'bg-gray-200' : ''} hover:bg-gray-200 duration-300 cursor-pointer min-w-52 rounded-md py-1 px-1`}>
+                            <ul className="flex flex-col gap-2 min-w-full w-max text-xl md:grow">
+                                <li id="country" onTouchEnd={(e) => setSubMenu(e.currentTarget.id)} onMouseEnter={(e) => setSubMenu(e.currentTarget.id)} className={`flex gap-3 items-center ${subMenu === 'country' ? 'bg-gray-200' : ''} hover:bg-gray-200 duration-300 cursor-pointer min-w-52 rounded-md py-1 px-1`}>
                                     <i className="fi fi-rs-flag size-5 box-content text-emerald-800 bg-emerald-800/40 p-2 rounded-lg"></i>
                                     <span className="text-gray-800 text-lg">کشور ها</span>
                                     <i className="fi fi-rs-angle-left size-3 text-xs mr-auto"></i>
                                 </li>
-                                <li id="category" onMouseEnter={(e) => setSubMenu(e.currentTarget.id)} className={`flex gap-3 items-center ${subMenu === 'category' ? 'bg-gray-200' : ''} hover:bg-gray-200 duration-300 cursor-pointer min-w-52 rounded-md py-1 px-1`}>
+                                <li id="category" onTouchEnd={(e) => setSubMenu(e.currentTarget.id)} onMouseEnter={(e) => setSubMenu(e.currentTarget.id)} className={`flex gap-3 items-center ${subMenu === 'category' ? 'bg-gray-200' : ''} hover:bg-gray-200 duration-300 cursor-pointer min-w-52 rounded-md py-1 px-1`}>
                                     <i className="fi fi-rs-category size-5 box-content text-orange-600 bg-orange-600/40 p-2 rounded-lg"></i>
                                     <span className="text-gray-800 text-lg">دسته بندی ها</span>
                                     <i className="fi fi-rs-angle-left size-3 text-xs mr-auto"></i>
@@ -89,53 +91,55 @@ export default function HasSubMenu({ title }: Props) {
                                 </Link>
                             </ul>
                         </div>
-                        <div className="w-max min-w-48 bg-stone-50 rounded-lg min-h-96 p-3">
-                            <p className="text-sm font-semibold text-gray-700">{subMenu === 'country' ? 'کشورها' : 'دسته بندی ها'}</p>
-                            <SubMenuList active={subMenu === 'country'}>
-                                <li className="flex items-center">
-                                    <Image className="rounded-md size-8" alt="" src={`/flags/1x1/de.svg`} height={40} width={40} />
-                                    <Link href={`/tours?countries[]=DE`}>آلمان</Link>
-                                </li>
-                                <li className="flex items-center">
-                                    <Image className="rounded-md size-8" alt="" src={`/flags/1x1/it.svg`} height={40} width={40} />
-                                    <Link href={`/tours?countries[]=IT`}>ایتالیا</Link>
-                                </li>
-                                <li className="flex items-center">
-                                    <Image className="rounded-md size-8" alt="" src={`/flags/1x1/fr.svg`} height={40} width={40} />
-                                    <Link href={`/tours?countries[]=FR`}>فرانسه</Link>
-                                </li>
-                                <li className="flex items-center">
-                                    <Image className="rounded-md size-8" alt="" src={`/flags/1x1/tr.svg`} height={40} width={40} />
-                                    <Link href={`/tours?countries[]=TR`}>ترکیه</Link>
-                                </li>
-                                <li className="flex items-center">
-                                    <Image className="rounded-md size-8" alt="" src={`/flags/1x1/th.svg`} height={40} width={40} />
-                                    <Link href={`/tours?countries[]=TH`}>تایلند</Link>
-                                </li>
-                            </SubMenuList>
-                            <SubMenuList active={subMenu === 'category'}>
-                                <li className="flex items-center">
-                                    <Image className="rounded-full size-8" alt="" src={`/images/categories/forest.svg`} height={40} width={40} />
-                                    <Link href={`/tours`}>جنگل و بافت طبیعی</Link>
-                                </li>
-                                <li className="flex items-center">
-                                    <Image className="rounded-full size-8" alt="" src={`/images/categories/desert.svg`} height={40} width={40} />
-                                    <Link href={`/tours`}>صحرایی</Link>
-                                </li>
-                                <li className="flex items-center">
-                                    <Image className="rounded-full size-8" alt="" src={`/images/categories/mountain.svg`} height={40} width={40} />
-                                    <Link href={`/tours`}>کوهستانی</Link>
-                                </li>
-                                <li className="flex items-center">
-                                    <Image className="rounded-full size-8" alt="" src={`/images/categories/beach.svg`} height={40} width={40} />
-                                    <Link href={`/tours`}>ساحلی</Link>
-                                </li>
-                                <li className="flex items-center">
-                                    <Image className="rounded-full size-8" alt="" src={`/images/categories/city.svg`} height={40} width={40} />
-                                    <Link href={`/tours`}>شهری</Link>
-                                </li>
-                            </SubMenuList>
-                        </div>
+                        {subMenu && (
+                            <div className="fixed top-0 right-0 flex flex-col justify-center md:justify-start md:relative z-[101] w-4/5 md:w-max md:min-w-48 bg-stone-50 rounded-lg shadow-lg min-h-full md:min-h-96 p-3">
+                                <p className="text-sm font-semibold text-gray-700">{subMenu === 'country' ? 'کشورها' : 'دسته بندی ها'}</p>
+                                <SubMenuList active={subMenu === 'country'}>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-md size-8" alt="" src={`/flags/1x1/de.svg`} height={40} width={40} />
+                                        <Link href={`/tours?countries[]=DE`}>آلمان</Link>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-md size-8" alt="" src={`/flags/1x1/it.svg`} height={40} width={40} />
+                                        <Link href={`/tours?countries[]=IT`}>ایتالیا</Link>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-md size-8" alt="" src={`/flags/1x1/fr.svg`} height={40} width={40} />
+                                        <Link href={`/tours?countries[]=FR`}>فرانسه</Link>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-md size-8" alt="" src={`/flags/1x1/tr.svg`} height={40} width={40} />
+                                        <Link href={`/tours?countries[]=TR`}>ترکیه</Link>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-md size-8" alt="" src={`/flags/1x1/th.svg`} height={40} width={40} />
+                                        <Link href={`/tours?countries[]=TH`}>تایلند</Link>
+                                    </li>
+                                </SubMenuList>
+                                <SubMenuList active={subMenu === 'category'}>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-full size-8" alt="" src={`/images/categories/forest.svg`} height={40} width={40} />
+                                        <Link href={`/tours`}>جنگل و بافت طبیعی</Link>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-full size-8" alt="" src={`/images/categories/desert.svg`} height={40} width={40} />
+                                        <Link href={`/tours`}>صحرایی</Link>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-full size-8" alt="" src={`/images/categories/mountain.svg`} height={40} width={40} />
+                                        <Link href={`/tours`}>کوهستانی</Link>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-full size-8" alt="" src={`/images/categories/beach.svg`} height={40} width={40} />
+                                        <Link href={`/tours`}>ساحلی</Link>
+                                    </li>
+                                    <li className="flex items-center">
+                                        <Image className="rounded-full size-8" alt="" src={`/images/categories/city.svg`} height={40} width={40} />
+                                        <Link href={`/tours`}>شهری</Link>
+                                    </li>
+                                </SubMenuList>
+                            </div>
+                        )}
                     </div>
                 </div>
             )}
