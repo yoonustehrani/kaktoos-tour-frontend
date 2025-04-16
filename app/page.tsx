bf8514b812e8a26f3d2f76e9ac09293f1205059d
yoonustehrani <yoonustehrani28@gmail.com>
2025-04-16 12:09:02 +0330

    categories section responsive

--- app/page.tsx
+++ app/page.tsx
@@ -16,83 +16,74 @@ export default function Home() {
                 <SuggestedDestinationsSection />
             </section>
             <PopularDestinationsSection />
-            <section className="bg-websiteGreen w-full px-4 h-96 my-10 flex flex-col items-center gap-3 py-8">
+            <section className="bg-websiteGreen w-full md:px-4 h-fit md:my-10 flex flex-col items-center gap-3 py-8">
                 <h3 className="text-white font-bold text-4xl">دسته بندی تورها</h3>
-                <p className="mb-6 text-gray-300">بر اساس ویژگی ها موردنظر شما برای مقصد تور</p>
-                <div className="py-4 px-6 w-4/5 flex flex-wrap justify-center gap-20">
-                    <Link href={`/tours`} className="flex flex-col gap-2 items-center">
-                        <div className="overflow-hidden flex items-center justify-center rounded-full size-40">
-                            <Image
-                                src="/images/categories/forest.svg"
-                                className="w-full h-full object-cover"
-                                alt="Forest category"
-                                width={0}
-                                height={0}
-                            />
-                        </div>
-                        <span className="text-white text-xl font-semibold">جنگل و بافت طبیعی</span>
-                    </Link>
-                    <Link href={`/tours`} className="flex flex-col gap-2 items-center">
-                        <div className="overflow-hidden flex items-center justify-center rounded-full size-40">
-                            <Image
-                                src="/images/categories/desert.svg"
-                                className="w-full h-full object-cover"
-                                alt="Forest category"
-                                width={0}
-                                height={0}
-                            />
-                        </div>
-                        <span className="text-white text-xl font-semibold">صحرایی</span>
-                    </Link>
-                    <Link href={`/tours`} className="flex flex-col gap-2 items-center">
-                        <div className="overflow-hidden flex items-center justify-center rounded-full size-40">
-                            <Image
-                                src="/images/categories/mountain.svg"
-                                className="w-full h-full object-cover"
-                                alt="Forest category"
-                                width={0}
-                                height={0}
-                            />
-                        </div>
-                        <span className="text-white text-xl font-semibold">کوهستانی</span>
-                    </Link>
-                    <Link href={`/tours`} className="flex flex-col gap-2 items-center">
-                        <div className="overflow-hidden flex items-center justify-center rounded-full size-40">
-                            <Image
-                                src="/images/categories/beach-2.svg"
-                                className="w-full h-full object-cover"
-                                alt="Forest category"
-                                width={0}
-                                height={0}
-                            />
-                        </div>
-                        <span className="text-white text-xl font-semibold">ساحلی</span>
-                    </Link>
-                    <Link href={`/tours`} className="flex flex-col gap-2 items-center">
-                        <div className="overflow-hidden flex items-center justify-center rounded-full size-40">
-                            <Image
-                                src="/images/categories/city.svg"
-                                className="w-full h-full object-cover"
-                                alt="Forest category"
-                                width={0}
-                                height={0}
-                            />
-                        </div>
-                        <span className="text-white text-xl font-semibold">شهری</span>
-                    </Link>
-                    <Link href={`/tours`} className="flex flex-col gap-2 items-center">
-                        <div className="overflow-hidden flex items-center justify-center rounded-full size-40">
-                            <Image
-                                src="/images/categories/country-2.svg"
-                                className="w-full h-full object-cover"
-                                alt="Forest category"
-                                width={0}
-                                height={0}
-                            />
-                        </div>
-                        <span className="text-white text-xl font-semibold">برون شهری</span>
-                    </Link>
+                <p className="md:mb-6 text-gray-300">بر اساس ویژگی ها موردنظر شما برای مقصد تور</p>
+                <div className="overflow-x-auto snap-center w-full">
+                    <div className="py-4 md:px-6 grid grid-flow-col grid-rows-2 gap-x-6 gap-y-3 w-max min-w-full px-4 md:w-4/5 md:flex flex-wrap justify-center md:gap-y-10 md:gap-x-20">
+                        <Link href={`/tours`} className="flex flex-col gap-2 items-center">
+                            <div className="overflow-hidden flex items-center justify-center rounded-full size-20 md:size-40">
+                                <Image
+                                    src="/images/categories/forest.svg"
+                                    className="w-full h-full object-cover"
+                                    alt="Forest category"
+                                    width={0}
+                                    height={0}
+                                />
+                            </div>
+                            <span className="text-white text-sm md:text-xl font-semibold">جنگل و بافت طبیعی</span>
+                        </Link>
+                        <Link href={`/tours`} className="flex flex-col gap-2 items-center">
+                            <div className="overflow-hidden flex items-center justify-center rounded-full size-20 md:size-40">
+                                <Image
+                                    src="/images/categories/mountain.svg"
+                                    className="w-full h-full object-cover"
+                                    alt="Forest category"
+                                    width={0}
+                                    height={0}
+                                />
+                            </div>
+                            <span className="text-white text-sm md:text-xl font-semibold">کوهستانی</span>
+                        </Link>
+                        <Link href={`/tours`} className="flex flex-col gap-2 items-center">
+                            <div className="overflow-hidden flex items-center justify-center rounded-full size-20 md:size-40">
+                                <Image
+                                    src="/images/categories/beach-2.svg"
+                                    className="w-full h-full object-cover"
+                                    alt="Forest category"
+                                    width={0}
+                                    height={0}
+                                />
+                            </div>
+                            <span className="text-white text-sm md:text-xl font-semibold">ساحلی</span>
+                        </Link>
+                        <Link href={`/tours`} className="flex flex-col gap-2 items-center">
+                            <div className="overflow-hidden flex items-center justify-center rounded-full size-20 md:size-40">
+                                <Image
+                                    src="/images/categories/city.svg"
+                                    className="w-full h-full object-cover"
+                                    alt="Forest category"
+                                    width={0}
+                                    height={0}
+                                />
+                            </div>
+                            <span className="text-white text-sm md:text-xl font-semibold">شهری</span>
+                        </Link>
+                        <Link href={`/tours`} className="flex flex-col gap-2 items-center">
+                            <div className="overflow-hidden flex items-center justify-center rounded-full size-20 md:size-40">
+                                <Image
+                                    src="/images/categories/country-2.svg"
+                                    className="w-full h-full object-cover"
+                                    alt="Forest category"
+                                    width={0}
+                                    height={0}
+                                />
+                            </div>
+                            <span className="text-white text-sm md:text-xl font-semibold">برون شهری</span>
+                        </Link>
+                    </div>
                 </div>
+                
             </section>
             <ToursOnCalendarSection />
         </>
